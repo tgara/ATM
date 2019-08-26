c0fef4723eee5469d805a2a455413997cb7b8e91
talel.gara <talel.gara@gmail.com>
2019-08-26 04:01:14 +0100

    model test

---
+++ ATM/Controllers/CompteController.cs
@@ -0,0 +1,94 @@
+﻿using ATM.Models;
+using System.Web.Mvc;
+
+namespace ATM.Controllers
+{
+    public class CompteController : Controller
+    {
+        // GET: Compte
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Compte/Details/5
+        public ActionResult Details()
+        {
+            var verifCompte = new VerificationCompte
+            {
+                NumeroCompte = "00000012345648",
+                Nom = "Foulen",
+                Prenom = "Foulen",
+                Solde = 54895,
+                Id = 1
+            };
+            return View(verifCompte);
+        }
+
+        // GET: Compte/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Compte/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Compte/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Compte/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Compte/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Compte/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ ATM/Models/VerificationCompte.cs
@@ -0,0 +1,14 @@
+﻿namespace ATM.Models
+{
+    public class VerificationCompte
+    {
+        public int Id { get; set; }
+        public string NumeroCompte { get; set; }
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public decimal Solde { get; set; }
+
+
+
+    }
+}
