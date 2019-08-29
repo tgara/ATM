fef3f718e79e7cf9566995faf8aa1fdf7aee816a
talel.gara <talel.gara@gmail.com>
2019-08-30 00:43:53 +0100

    day 3

--- ATM/Models/VerificationCompte.cs
+++ ATM/Models/VerificationCompte.cs
@@ -1,11 +1,18 @@
-﻿namespace ATM.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ATM.Models
 {
     public class VerificationCompte
     {
         public int Id { get; set; }
+
+        [Display(Name = "Numero de compte")]
         public string NumeroCompte { get; set; }
         public string Nom { get; set; }
         public string Prenom { get; set; }
+
+        public string NomPrenom { get { return Nom + "" + Prenom; } }
+        [DataType(DataType.Currency)]
         public decimal Solde { get; set; }
 
 
